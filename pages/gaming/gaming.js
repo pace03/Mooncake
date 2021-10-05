c5894c4d90c0730b9cafd7b441e8b67ba031af4a
pace03 <xzk_k_k@163.com>
2021-10-05 14:16:44 +0800

    历史记录页面初步完成

--- pages/gaming/gaming.js
+++ pages/gaming/gaming.js
@@ -3,5 +3,9 @@ Page({
     data: {
     },
     // 事件处理函数
-  })
-  
+  gohistory: function () {
+    wx.navigateTo({
+      url: '/pages/history/history'
+    })
+  },
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,6 @@
 // index.js
 Page({
   data: {
-   flag:true,
-   name:'',
-   Chinese_score: '',
-   Math_score:'',
-   average:''
   },
   // 事件处理函数
   gogaming: function () {
@@ -13,30 +8,4 @@ Page({
       url: '/pages/gaming/gaming'
     })
   },
-  nameInput:function(e){
-    this.setData({
-      name:e.detail.value
-    });
-  },
-  ChineseInput:function(e){
-    this.setData({
-      Chinese_score:e.detail.value
-    });
-  },
-  MathInput:function(e){
-    this.setData({
-      Math_score:e.detail.value
-    });
-  },
-  mysubmit:function(){
-    if(this.data.Chinese_score==''||this.data.Math_score==''||this.data.name==''){
-      return;
-    }else{
-      var avg=(this.data.Chinese_score*1+this.data.Math_score*1)/2;
-      this.setData({
-        flag: false,
-        average:avg,
-      });
-    }
-  }
 })
