1378e8272430784fbe9b7729b297bffc1bf2ff87
wzlwzl123456 <894477426@qq.com>
2021-10-06 00:00:10 +0800

    完成了游戏页面美化和随机数生成

--- pages/gaming/gaming.js
+++ pages/gaming/gaming.js
@@ -1,11 +1,31 @@
 // index.js
 Page({
     data: {
+      dice:[0,0,0,0,0,0],
+      num:[0,0,0,0,0,0]
     },
     // 事件处理函数
-  gohistory: function () {
+    gohistory() {
     wx.navigateTo({
       url: '/pages/history/history'
     })
-  },
+    },
+    // judge:function(){
+    //   var temp_str0='dice[0]';
+    //    this.setData({
+    //      'num[${}].value':this.data.num[0]+1
+    //    })
+    // },
+    gorandom(){
+      this.setData({
+        'dice[0]':Math.floor(Math.random()*6)+1,
+        'dice[1]':Math.floor(Math.random()*6)+1,
+        'dice[2]':Math.floor(Math.random()*6)+1,
+        'dice[3]':Math.floor(Math.random()*6)+1,
+        'dice[4]':Math.floor(Math.random()*6)+1,
+        'dice[5]':Math.floor(Math.random()*6)+1
+      })
+      // this.judge()
+    }
+      
 })
