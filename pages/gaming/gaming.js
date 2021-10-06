323928b1cc1aae90f74f2425834cc1ef7f6884d4
pace03 <xzk_k_k@163.com>
2021-10-07 00:04:17 +0800

    实现每按一次博饼按钮，6个骰子给出相应数字结果

--- pages/gaming/gaming.js
+++ pages/gaming/gaming.js
@@ -4,7 +4,13 @@ Page({
     dice:[0,0,0,0,0,0],
     num:[0,0,0,0,0,0],
     level:0,// level 0:无奖  level 1：一秀；  level 2：二举； level 3：四进； level 4：三红； level 5：对堂； level 6：状元,
-    result:'白给'
+    result:'白给',
+    flag1:0,
+    flag2:0,
+    flag3:0,
+    flag4:0,
+    flag5:0,
+    flag6:0,
   },
   // 事件处理函数
   gohistory() {
@@ -71,7 +77,213 @@ Page({
       default:
         break
     }
-
+ 
+    switch(this.data.dice[0]){
+      case 1:
+        this.setData({
+         flag1:1
+        })
+        break;
+      case 2:
+        this.setData({
+          flag1:2
+        })
+        break;
+      case 3:
+        this.setData({
+          flag1:3
+        })
+        break;
+      case 4:
+        this.setData({
+          flag1:4
+        })
+        break;
+      case 5:
+        this.setData({
+          flag1:5
+        })
+        break;
+      case 6:
+        this.setData({
+          flag1:6
+        })
+        break;
+      default:
+        this.setData({
+          flag1:0
+        })
+  }
+  switch(this.data.dice[1]){
+    case 1:
+      this.setData({
+       flag2:1
+      })
+      break;
+    case 2:
+      this.setData({
+        flag2:2
+      })
+      break;
+    case 3:
+      this.setData({
+        flag2:3
+      })
+      break;
+    case 4:
+      this.setData({
+        flag2:4
+      })
+      break;
+    case 5:
+      this.setData({
+        flag2:5
+      })
+      break;
+    case 6:
+      this.setData({
+        flag2:6
+      })
+      break;
+    default:
+      break
+}
+switch(this.data.dice[2]){
+  case 1:
+    this.setData({
+     flag3:1
+    })
+    break;
+  case 2:
+    this.setData({
+      flag3:2
+    })
+    break;
+  case 3:
+    this.setData({
+      flag3:3
+    })
+    break;
+  case 4:
+    this.setData({
+      flag3:4
+    })
+    break;
+  case 5:
+    this.setData({
+      flag3:5
+    })
+    break;
+  case 6:
+    this.setData({
+      flag3:6
+    })
+    break;
+  default:
+    break
+}
+switch(this.data.dice[3]){
+  case 1:
+    this.setData({
+     flag4:1
+    })
+    break;
+  case 2:
+    this.setData({
+      flag4:2
+    })
+    break;
+  case 3:
+    this.setData({
+      flag4:3
+    })
+    break;
+  case 4:
+    this.setData({
+      flag4:4
+    })
+    break;
+  case 5:
+    this.setData({
+      flag4:5
+    })
+    break;
+  case 6:
+    this.setData({
+      flag4:6
+    })
+    break;
+  default:
+    break
+}
+switch(this.data.dice[4]){
+  case 1:
+    this.setData({
+     flag5:1
+    })
+    break;
+  case 2:
+    this.setData({
+      flag5:2
+    })
+    break;
+  case 3:
+    this.setData({
+      flag5:3
+    })
+    break;
+  case 4:
+    this.setData({
+      flag5:4
+    })
+    break;
+  case 5:
+    this.setData({
+      flag5:5
+    })
+    break;
+  case 6:
+    this.setData({
+      flag5:6
+    })
+    break;
+  default:
+    break
+}
+switch(this.data.dice[5]){
+  case 1:
+    this.setData({
+     flag6:1
+    })
+    break;
+  case 2:
+    this.setData({
+      flag6:2
+    })
+    break;
+  case 3:
+    this.setData({
+      flag6:3
+    })
+    break;
+  case 4:
+    this.setData({
+      flag6:4
+    })
+    break;
+  case 5:
+    this.setData({
+      flag6:5
+    })
+    break;
+  case 6:
+    this.setData({
+      flag6:6
+    })
+    break;
+  default:
+    break
+}
   },
   gorandom(){
     this.data.level=0
@@ -92,9 +304,11 @@ Page({
     for(let i=0;i<6;i++){
       this.data.num[this.data.dice[i]-1]+=1;
     }
+    
     this.judge()
     console.log(this.data.dice)
     console.log(this.data.level)
     console.log(this.data.result)
   }
-})
+},
+)
