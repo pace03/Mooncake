f88b2f8f25c31b1dabe6f2c01766f7dfb3a5fff8
wzlwzl123456 <894477426@qq.com>
2021-10-06 17:46:57 +0800

    完成了摇骰子结果统计

--- pages/gaming/gaming.js
+++ pages/gaming/gaming.js
@@ -1,31 +1,34 @@
 // index.js
 Page({
-    data: {
-      dice:[0,0,0,0,0,0],
-      num:[0,0,0,0,0,0]
-    },
-    // 事件处理函数
-    gohistory() {
-    wx.navigateTo({
-      url: '/pages/history/history'
-    })
-    },
-    // judge:function(){
-    //   var temp_str0='dice[0]';
-    //    this.setData({
-    //      'num[${}].value':this.data.num[0]+1
-    //    })
-    // },
-    gorandom(){
-      this.setData({
-        'dice[0]':Math.floor(Math.random()*6)+1,
-        'dice[1]':Math.floor(Math.random()*6)+1,
-        'dice[2]':Math.floor(Math.random()*6)+1,
-        'dice[3]':Math.floor(Math.random()*6)+1,
-        'dice[4]':Math.floor(Math.random()*6)+1,
-        'dice[5]':Math.floor(Math.random()*6)+1
-      })
-      // this.judge()
+  data: {
+    dice:[0,0,0,0,0,0],
+    num:[0,0,0,0,0,0]
+  },
+  // 事件处理函数
+  gohistory() {
+  wx.navigateTo({
+    url: '/pages/history/history'
+  })
+  },
+  judge:function(){
+    
+  },
+  gorandom(){
+    for(let i=0;i<6;i++){
+      this.data.num[i]=0;
     }
-      
+    this.setData({
+      'dice[0]':Math.floor(Math.random()*6)+1,
+      'dice[1]':Math.floor(Math.random()*6)+1,
+      'dice[2]':Math.floor(Math.random()*6)+1,
+      'dice[3]':Math.floor(Math.random()*6)+1,
+      'dice[4]':Math.floor(Math.random()*6)+1,
+      'dice[5]':Math.floor(Math.random()*6)+1
+    })
+    for(let i=0;i<6;i++){
+      this.data.num[this.data.dice[i]-1]+=1;
+    }
+    this.judge()
+    console.log(this.data.num)
+  }
 })
